bc3639125707a4546b17de1575f82b3c7fbbd133
Nemsara Gamage <113989584+MGNemsara@users.noreply.github.com>
2024-06-03 23:17:25 +0530

    updated

---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../../components/styles.css';
+
+const mainPage = () => {
+
+    return(
+        <>
+
+         {/* <!-- navigation bar --> */}
+        <div className="navbar">
+            <a href="#" className="logo">MovieBlocks</a>
+            
+        </div>
+
+        </>
+    )
+}
+
+export default mainPage;
